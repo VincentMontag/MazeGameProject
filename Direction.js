90258487ec4eb0926b7c0f4b09522145569cdd3b
Vincent <104990196+VincentMontag@users.noreply.github.com>
2022-12-12 14:37:01 +0100

    Update and rename Direction.java to Direction.js

---
+++ Direction.js
@@ -0,0 +1,30 @@
+/**
+ * Get all allowed enum values: Object.keys(Direction) 
+   returns an array ['Up', 'Down', 'Left', 'Right']
+ * Check if a value equals an enum value: val === Direction.Up.name
+ * Check if a value is in the enum: Direction.Up instanceof Direction
+ */ 
+class Direction{
+	
+	static RIGHT = new Direction(0);
+	static UP = new Direction(1);
+	static LEFT = new Direction(2);
+	static DOWN = new Direction(3);
+	
+ 	constructor(num){
+ 	
+ 		this.num = num;
+	}
+	
+ 	left(){
+ 		if(this.num == 0){
+ 			return Direction.UP;
+ 		} else if(this.num == 1){
+ 			return Direction.LEFT;
+ 		} else if(this.num == 2){
+ 			return Direction.DOWN;
+ 		}else if(this.num == 3){
+ 			return Direction.RIGHT;
+ 		}
+ 	}
+}
