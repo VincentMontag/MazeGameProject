cf9acaea82e52dbb6b70cf98d7a57b765cef1746
lelan <lelanz21042003@gmail.com>
2023-01-04 18:00:43 +0100

    adjusted size

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 // Maze.js
 const maze = require("./Maze.js"); 
 
-const mazeFields = maze.generateMaze(50, 40);
+const mazeFields = maze.generateMaze(30, 15);
 
 var Direction = require('./Direction.js');
  
