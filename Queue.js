56267a84a7d340e2cfe61f0018d71d8732f3625a
lelan <lelanz21042003@gmail.com>
2023-01-07 19:32:50 +0100

    Implemented queue

--- Queue.js
+++ Queue.js
@@ -1,1 +1,21 @@
+let queue = [];
 
+function offer(element) {
+	queue.push(element);
+}
+
+function peek() {
+	return queue[0];
+}
+
+function poll() {
+	let toReturn = queue[0];
+	delete queue[0];
+	return toReturn;
+}
+
+function empty() {
+	return queue.length == 0;
+}
+
+module.exports = {offer, peek, poll, empty};
