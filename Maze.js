7be3f6ad162ec7cb9727f57eef588e9cbc76319d
Benedict Lelanz <105001351+BenedictLelanz@users.noreply.github.com>
2023-01-10 10:19:00 +0100

    Update Maze.js

--- Maze.js
+++ Maze.js
@@ -1,6 +1,8 @@
 
 const Direction = require('./Direction.js');
 
+const Player = require('./Player.js');
+
 const Queue = require('./Queue.js');
 
 let players = {};
@@ -14,45 +16,6 @@ let solPath = [];
 let maze = [];
 let w = 0;
 let h = 0;
-
-class Player {
-	
-	constructor(x, y, name) {
-		this.x = x;
-		this.y = y;
-		this.dir = Direction.RIGHT;
-		this.stepsNeeded = 0;
-		this.done = false;
-		this.name = name;
-	}
-	
-	// Moves the player if it is allowed.
-	// dir is a matching string
-	move(direction) {
-		if (!this.done && isAllowed(this.x, this.y, direction)) {
-			this.dir = direction;
-			if (direction.equal(Direction.UP)) this.y--;
-			else if (direction.equal(Direction.LEFT)) this.x--;
-			else if (direction.equal(Direction.RIGHT)) this.x++;
-			else if (direction.equal(Direction.DOWN)) this.y++;
-			if (this.x == w + 1) {
-				let key = {
-					username: this.name,
-					time: Date.now()
-				}
-				this.done = true;
-				highscores[JSON.stringify(key)] = {
-					steps: this.stepsNeeded, 
-					score: Math.floor(shortestPath / this.stepsNeeded * 100)
-				};
-			}
-			this.stepsNeeded++;
-			return true;
-		}
-		return false;
-	}
-	
-}
 
 function addPlayer(id, name) {
 	players[id] = new Player(0, 1, name);
