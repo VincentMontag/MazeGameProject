50b111d5a717d765e3457c1736c5878979766e6f
Benedict Lelanz <105001351+BenedictLelanz@users.noreply.github.com>
2023-01-10 10:17:11 +0100

    Update Player.js

--- Player.js
+++ Player.js
@@ -1,1 +1,38 @@
-
+class Player {
+	
+	constructor(x, y, name) {
+		this.x = x;
+		this.y = y;
+		this.dir = Direction.RIGHT;
+		this.stepsNeeded = 0;
+		this.done = false;
+		this.name = name;
+	}
+	
+	// Moves the player if it is allowed.
+	// dir is a matching string
+	move(direction) {
+		if (!this.done && isAllowed(this.x, this.y, direction)) {
+			this.dir = direction;
+			if (direction.equal(Direction.UP)) this.y--;
+			else if (direction.equal(Direction.LEFT)) this.x--;
+			else if (direction.equal(Direction.RIGHT)) this.x++;
+			else if (direction.equal(Direction.DOWN)) this.y++;
+			if (this.x == w + 1) {
+				let key = {
+					username: this.name,
+					time: Date.now()
+				}
+				this.done = true;
+				highscores[JSON.stringify(key)] = {
+					steps: this.stepsNeeded, 
+					score: Math.floor(shortestPath / this.stepsNeeded * 100)
+				};
+			}
+			this.stepsNeeded++;
+			return true;
+		}
+		return false;
+	}
+	
+}
